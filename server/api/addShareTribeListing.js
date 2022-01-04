fc1bebb8e50a08b9bd01cbe73bf16faff563f2f7
ujjawalgoyal <ujjawalgoyal00000@gmail.com>
2022-01-04 10:47:50 +0530

    add status check and save data in sharetribe after binding the keys

--- server/api/addShareTribeListing.js
+++ server/api/addShareTribeListing.js
@@ -3,9 +3,18 @@ const { getSdk, handleError, serialize } = require('../api-util/sdk');
 module.exports = (req, res) => {
     const sdk = getSdk(req, res);
 
-    sdk.ownListings.create({ paveApiName: req.body }, { expand: true }).then(ress => {
-        res.status(200).send(ress.data)
-    }).catch(err => {
-        handleError(res, err)
-    })
+    if (req.body.session && req.body.session.status && req.body.session.status === "COMPLETE") {
+        let getResult = req.body.session
+        let createObj = {
+            ...getResult,
+            brand: getResult.vehicle.make,
+            title: getResult.vehicle.year + getResult.vehicle.make + getResult.vehicle.model,
+            description: Object.values(getResult.vehicle).join(", ")
+        }
+        sdk.ownListings.create(createObj, { expand: true }).then(ress => {
+            res.status(200).send(ress.data)
+        }).catch(err => {
+            handleError(res, err)
+        })
+    }
 };
