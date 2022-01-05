56813a3407d2f26824b2327febb6dc1e79af72e0
ujjawalgoyal <ujjawalgoyal00000@gmail.com>
2022-01-05 12:23:42 +0530

    handle for not matchind session key

--- server/api/addShareTribeListing.js
+++ server/api/addShareTribeListing.js
@@ -8,39 +8,29 @@ module.exports = (req, res) => {
         integrationSdk.listings.query().then(ress => {
             let newArray = ress.data.data || []
             let findList = newArray.filter(ele => (ele.attributes.publicData.session_key === getResult.session_key))[0]
-            let updateObj = {
-                id: new UUID(findList.id.uuid),
-                // brand: getResult.vehicle && getResult.vehicle.make,
-                title: getResult.vehicle.year + getResult.vehicle.make + getResult.vehicle.model,
-                description: Object.values(getResult.vehicle).join(", "),
-                publicData: { ...getResult }
-            }
-            integrationSdk.listings.update(updateObj).then(out => {
-                res.status(200).send(out.data)
+            if (findList) {
 
-            }).catch(e => {
-                handleError(res, e)
-            })
+                let updateObj = {
+                    id: new UUID(findList.id.uuid),
+                    // brand: getResult.vehicle && getResult.vehicle.make,
+                    title: getResult.vehicle.year + getResult.vehicle.make + getResult.vehicle.model,
+                    description: Object.values(getResult.vehicle).join(", "),
+                    publicData: { ...getResult }
+                }
+                integrationSdk.listings.update(updateObj).then(out => {
+                    res.status(200).send(out.data)
+
+                }).catch(e => {
+                    handleError(res, e)
+                })
+            } else {
+                res.status(500).send("No Session found.")
+            }
         }).catch(err => {
             handleError(res, err)
         })
-
     }
-
-    // if (req.body.session && req.body.session.status && req.body.session.status === "COMPLETE") {
-    //     let getResult = req.body.session;
-    //     let createObj = {
-    //         ...getResult,
-    //         brand: getResult.vehicle.make,
-    //         title: getResult.vehicle.year + getResult.vehicle.make + getResult.vehicle.model,
-    //         description: Object.values(getResult.vehicle).join(", ")
-    //     };
-    //     sdk.ownListings.create(createObj, { expand: true }).then(ress => {
-    //         res.status(200).send(ress.data)
-    //     }).catch(err => {
-    //         handleError(res, err)
-    //     })
-    // } else {
-    //     res.status(500).send("No Session found.")
-    // }
+    else {
+        res.status(500).send("No Session found.")
+    }
 };
