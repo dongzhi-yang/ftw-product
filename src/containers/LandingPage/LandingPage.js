4db69abb59b42c7395f2b7897f9a0f9ae0d4fc4c
Gaurav <gauravgargcs@gmail.com>
2022-02-24 22:23:02 +0530

    landing page

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -8,30 +8,86 @@ import config from '../../config';
 import { injectIntl, intlShape } from '../../util/reactIntl';
 import { isScrollingDisabled } from '../../ducks/UI.duck';
 
-import '../../../src/App.css';
+import {
+  Page,
+  LayoutSingleColumn,
+  LayoutWrapperTopbar,
+  LayoutWrapperMain,
+  LayoutWrapperFooter,
+} from '../../components';
+import TopbarContainer from '../../containers/TopbarContainer/TopbarContainer';
 
+import facebookImage from '../../assets/sneakertimeFacebook-1200x630.jpg';
+import twitterImage from '../../assets/sneakertimeTwitter-600x314.jpg';
+
+import SectionHero from './SectionHero/SectionHero';
+import SectionHowItWorks from './SectionHowItWorks/SectionHowItWorks';
+import SectionFilteredSearches from './SectionFilteredSearches/SectionFilteredSearches';
+import css from './LandingPage.module.css';
+import '../../../src/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 export const LandingPageComponent = props => {
-	const { history, intl, location, scrollingDisabled } = props;
-	 const siteTitle = config.siteTitle;
+  const { history, intl, location, scrollingDisabled } = props;
+
+  // Schema for search engines (helps them to understand what this page is about)
+  // http://schema.org
+  // We are using JSON-LD format
+  const siteTitle = config.siteTitle;
   const schemaTitle = intl.formatMessage({ id: 'LandingPage.schemaTitle' }, { siteTitle });
   const schemaDescription = intl.formatMessage({ id: 'LandingPage.schemaDescription' });
+  const schemaImage = `${config.canonicalRootURL}${facebookImage}`;
+
   return (
-      <>
-    
-      </>
-      );
+    <Page
+      className={css.root}
+      scrollingDisabled={scrollingDisabled}
+      contentType="website"
+      description={schemaDescription}
+      title={schemaTitle}
+      facebookImages={[{ url: facebookImage, width: 1200, height: 630 }]}
+      twitterImages={[
+        { url: `${config.canonicalRootURL}${twitterImage}`, width: 600, height: 314 },
+      ]}
+      schema={{
+        '@context': 'http://schema.org',
+        '@type': 'WebPage',
+        description: schemaDescription,
+        name: schemaTitle,
+        image: [schemaImage],
+      }}
+    >
+      <LayoutSingleColumn>
+        <LayoutWrapperTopbar>
+          <TopbarContainer />
+        </LayoutWrapperTopbar>
+        <LayoutWrapperMain>
+          <div className={css.heroContainer}>
+            <SectionHero
+              rootClassName={css.heroRoot}
+              className={css.hero}
+              history={history}
+              location={location}
+            />
+          </div>
+          <ul className={css.sections}>
+            <li className={css.section}>
+              <div className={css.sectionContentFirstChild}>
+                <SectionFilteredSearches />
+              </div>
+            </li>
+            <li className={css.section}>
+              <div className={css.sectionContent}>
+                <SectionHowItWorks />
+              </div>
+            </li>
+          </ul>
+        </LayoutWrapperMain>
+      </LayoutSingleColumn>
+    </Page>
+  );
 };
 
-
-// Note: it is important that the withRouter HOC is **outside** the
-// connect HOC, otherwise React Router won't rerender any Route
-// components since connect implements a shouldComponentUpdate
-// lifecycle hook.
-//
-// See: https://github.com/ReactTraining/react-router/issues/4671
 const { bool, object } = PropTypes;
 
 LandingPageComponent.propTypes = {
