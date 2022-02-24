0c9464b60803011603dd3f2a0ab897c9b897e174
Gaurav <gauravgargcs@gmail.com>
2022-02-25 00:35:48 +0530

    Landing

--- src/containers/LandingPage/my component/Content.js
+++ src/containers/LandingPage/my component/Content.js
@@ -13,6 +13,7 @@ import quote from '../../../assets/quote.png';
 //import backgroundimg from '../../../assets/qublurry-car-copyote.png';
 import config from '../../../config';
 import AOS from "aos";
+//import "aos/dist/aos.css";
 AOS.init();
 export default function Content()
 {
