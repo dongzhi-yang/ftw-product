ad38e13e0ea3014a780d3d001a5fe25644f3ad8a
Gaurav <gauravgargcs@gmail.com>
2022-02-25 15:30:45 +0530

    add favicon

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../../../src/App.css';
+import facebookImage from '../../assets/sneakertimeFacebook-1200x630.jpg';
+import twitterImage from '../../assets/sneakertimeTwitter-600x314.jpg'
 import Header from './my component/Header';
 import Footer from './my component/Footer';
 import Content from './my component/Content';
-
+import config from '../../config';
 export const LandingPageComponent = props => {
+	
+	  const siteTitle = config.siteTitle;
+ 
   return (
       <>
       <Header></Header>
